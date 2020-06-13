e920d13c50a9b478b4d063af0ba602ec89a56e55
mjrosenberg <mjr70@duke.edu>
2020-06-13 16:17:22 -0700

    fixed spelling

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -4,7 +4,7 @@ import Repo from './Repo.jsx';
 const RepoList = (props) => (
 
   <div id='repolist'>
-    <h4> Top 25 Repo's Based on Star Rating</h4>
+    <h4> Top 25 Repos Based on Star Rating</h4>
     {props.repos.map((repo)=>{
       return <Repo key={repo._id} repo={repo}/>
     })}
